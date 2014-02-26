6b7265e23e0e9f16afb12fb1b076fff45d10756c
Noj Yerac <noj.yerac@gmail.com>
2014-02-26 01:23:35 -0600

    Added some forms to admin.php

--- admin.php
+++ admin.php
@@ -1,4 +1,7 @@
 <?php
+require_once('config.php');
+require_once('db.php');
+require_once('filter.php');
 require_once('comp.php');
 require_once('creds.php');
 
@@ -10,17 +13,39 @@ if (isset($_SESSION['csrf_token'])) {
 
 $_SESSION['csrf_token'] = $new_csrf_token = get_csrf_token();
 
+$csrf_passed = (
+	isset($old_csrf_token) &&
+	isset($_POST['csrf_token'] &&
+	$old_csrf_token == $_POST['csrf_token']
+);
+
 if (!isset($_SESSION['user_priv']) || $_SESSION['user_priv'] != 'admin') {
 	http_response_code(401);
 	echo 'Not Authorized';
 	exit();
 }
 
+$add_user_form = formify(
+	inputify('hidden', 'csrf_token', array('value' => $new_csrf_token)),
+	inputify('text', 'new_user_login', array('label' => 'User name: ')),
+	inputify('password', 'new_user_pass', array('label' => 'Password: ')),
+	inputify('password', 'verify_new_user_pass', array('label' => 'Verify Password: ')),
+	inputify('submit', 'submit', array('value' => 'Create User'))
+);
+
+$new_filter_form = formify(
+	inputify('hidden', 'csrf_token', array('value' => $new_csrf_token)),
+	inputify('text', 'new_filter_regex', array('label' => 'Regex: ', 'placeholder' => '/(.*)/')),
+	inputify('text', 'new_filter_replacement', array('label' => 'Replacement: ', 'placeholder' => '$1')),
+	inputify('submit', 'submit', array('value' => 'Create Filter'))
+);
+
+
+
+
 echo 'Admin Page!';
 
 if (isset($_GET['action'])) {
-	$csrf_passed = (isset($old_csrf_token) &&
-		$old_csrf_token == $_POST['csrf_token']);
 	switch($_GET['action']) {
 	case 'add user':
 		// add user logic
