3d5e9f9cb79988c803c284578da5856bd33d28ed
Noj Yerac <noj.yerac@gmail.com>
2014-02-28 00:07:34 -0600

    added remove_filter_form

--- admin.php
+++ admin.php
@@ -4,6 +4,7 @@ require_once('db.php');
 require_once('filter.php');
 require_once('comp.php');
 require_once('creds.php');
+require_once('csrf.php');
 
 session_start();
 
@@ -15,7 +16,7 @@ $_SESSION['csrf_token'] = $new_csrf_token = get_csrf_token();
 
 $csrf_passed = (
 	isset($old_csrf_token) &&
-	isset($_POST['csrf_token'] &&
+	isset($_POST['csrf_token']) &&
 	$old_csrf_token == $_POST['csrf_token']
 );
 
@@ -26,20 +27,61 @@ if (!isset($_SESSION['user_priv']) || $_SESSION['user_priv'] != 'admin') {
 }
 
 $add_user_form = formify(
-	inputify('hidden', 'csrf_token', array('value' => $new_csrf_token)),
-	inputify('text', 'new_user_login', array('label' => 'User name: ')),
-	inputify('password', 'new_user_pass', array('label' => 'Password: ')),
-	inputify('password', 'verify_new_user_pass', array('label' => 'Verify Password: ')),
-	inputify('submit', 'submit', array('value' => 'Create User'))
+	'POST', BASE_URL . '/admin.php?action=add+user',
+	array(
+		inputify('hidden', 'csrf_token', array('value' => $new_csrf_token)),
+		inputify('text', 'new_user_login', array('label' => 'User name: ')),
+		inputify('password', 'new_user_pass', array('label' => 'Password: ')),
+		inputify('password', 'verify_new_user_pass', array('label' => 'Verify Password: ')),
+		inputify('submit', 'submit', array('value' => 'Create User'))
+	),
+	array()
 );
 
 $new_filter_form = formify(
-	inputify('hidden', 'csrf_token', array('value' => $new_csrf_token)),
-	inputify('text', 'new_filter_regex', array('label' => 'Regex: ', 'placeholder' => '/(.*)/')),
-	inputify('text', 'new_filter_replacement', array('label' => 'Replacement: ', 'placeholder' => '$1')),
-	inputify('submit', 'submit', array('value' => 'Create Filter'))
+	'POST', BASE_URL . '/admin.php?action=new+filter',
+	array(
+		inputify('hidden', 'csrf_token', array('value' => $new_csrf_token)),
+		inputify('text', 'new_filter_regex', array('label' => 'Regex: ', 'placeholder' => '/(.*)/')),
+		inputify('text', 'new_filter_replacement', array('label' => 'Replacement: ', 'placeholder' => '$1')),
+		inputify('submit', 'submit', array('value' => 'Create Filter'))
+	),
+	array()
 );
 
+function get_filter_selections() {
+	$query = 'SELECT id, regex, replacement  from filters';
+	$result = db_query($query, array());
+	$rows = db_all_results($result);
+	$selection_format = '<selection value="%s"><tr><td>%s</td><td>%s</td></tr></selection>';
+	$filter_selections = '<table id="filter_selections_table">';
+	foreach ($rows as $row) {
+		$filter_selections .=
+			sprintf($selection_format, $row['id'],
+				htmlentities($row['regex']),
+				htmlentities($row['replacement'])
+			);
+	}
+	$filter_selections .= '</table>';
+	return $filter_selections;
+}
+
+$remove_filter_form = formify(
+	'POST', BASE_URL . '/admin.php?action=remove+filter',
+	array(
+		inputify('hidden', 'csrf_token', array('value' => $new_csrf_token)),
+		tagify(
+			array(
+				'tag' => 'options',
+				'name' => 'remove_filter_id[]',
+				'multiselect' => 'multiselect',
+				'innerHTML' => get_filter_selections()
+			)
+		),
+		inputify('submit', 'submit', array('value' => 'Remove'))
+	),
+	array()
+);
 
 
 
